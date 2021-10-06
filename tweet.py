1837d7c534f9824985bbba93d1ea62c966066e55
Phani Adabala <adabal.phani@gmail.com>
2021-10-06 23:54:44 +0530

    Added SQL file Updated tweet.py

--- tweet.py
+++ tweet.py
@@ -1,15 +1,13 @@
 import argparse
-
-from ShiningArmor import twitter
-
+import json
+import logging
 import sqlite3
 from sqlite3 import Error
 
-import logging
-
+from ShiningArmor import twitter
 
 logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',
-                    datefmt='%m/%d/%Y %I:%M:%S %p',level=logging.DEBUG)
+                    datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
 
 
 def get_cmd_line_args():
@@ -24,65 +22,65 @@ def get_cmd_line_args():
 
     args = vars(ap.parse_args())
 
-    logging.info(f'Tokens file is - {args['tokens_file']}')
-    logging.info(f'Sqlite3 DB file is - {args['db_file']}')
-    logging.info(f'SQL file is - {args['sql_file']}')
+    logging.info(f'Tokens file is - {args["tokens_file"]}')
+    logging.info(f'Sqlite3 DB file is - {args["db_file"]}')
+    logging.info(f'SQL file is - {args["sql_file"]}')
 
     return args
 
 
-def get_sql(args):
+def db_conn(db_file):
+    # Get DB connection
     try:
-        fh = open(args['sql_file'], "r")
-        args['sql'] = fh.read()
+        db = sqlite3.connect(db_file)
     except Error as err:
-        logging.error(err)
-        args['rc'] = 1
+        raise err
 
-    return args
+    return db
 
 
-def get_msg(args):
-
-    # Get the SQL statement
-    args = get_sql(args['sql_file'])
-
-    if args['rc'] == 1:
-       return args
-
+def db_query(db, sql_stmt):
+    # Execute the SQL statement
     try:
-        # Create a database connection
-        args['db_conn'] = None
-        args['db_conn'] = sqlite3.connect(args['db_file'])
-
-        # Query the table
-        cur = args['db_conn'].cursor()
-        cur.execute(args['sql'])
-        args['record'] = cur.fetchone()
+        cur = db.cursor()
+        cur.execute(sql_stmt['select'])
+        rec = cur.fetchone()
     except Error as err:
-        logging.error(err)
-        args['rc'] = 1
+        raise err
 
-    return args
+    return rec
+
+
+def db_sql(sql_file):
+    # Get SQL statements - SELECT/UPDATE
+    try:
+        f = open(sql_file, mode='r')
+        sql_stmt = json.load(f)
+        f.close()
+    except FileNotFoundError as err:
+        raise err
+    except IOError as err:
+        raise err
+
+    return sql_stmt
 
 
 if __name__ == "__main__":
     args = get_cmd_line_args()
-    args['rc'] = 0
 
-    #Get tweet messge from database
-    args = get_msg(args)
+    # Get DB connection
+    db = db_conn(args['db_file'])
 
-    if args['rc'] == 0:
-        try:
-            tokens = twitter.tokens(args['tokens_file'])
-            api = twitter.auth(tokens)
-            twitter.tweet(api, msg)
-        except Error as err:
-            logging.error(err)
-            args['rc'] = 1
-    else:
-        logging.error(f'Unable to get messages to tweet')
+    # Get SQL statements
+    sql_stmt = db_sql(args['sql_file'])
 
-    return args['rc']
+    # Prepare & Execute SQL
+    rec = db_query(db, sql_stmt)
 
+    try:
+        tokens = twitter.tokens(args['tokens_file'])
+        api = twitter.auth(tokens)
+        twitter.tweet(api, rec['msg'])
+    except Error as err:
+        logging.error(err)
+        raise err
